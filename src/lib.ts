799023ca14ed9e8815a57974860e5948dbbbef67
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-06-26 01:45:17 +0900

    モバイル向けコールバック設定関連のコードとテストを追加・整理

--- src/lib.ts
+++ src/lib.ts
@@ -38,6 +38,8 @@ export const getJWEFromSetCookieHeader = (response: Response) => {
     ?.replace(`${config.authjsSessionName}=`, '')
 }
 
+const isMobileRedirectUrl = (url: URL | string) =>
+url.toString().startsWith(`${config.host}/mobile`)
 
 // UrsaAuth用にカスタマイズされたパラメータをチェックします
 //
@@ -47,29 +49,119 @@ export const getJWEFromSetCookieHeader = (response: Response) => {
 // callbackUrlに関連付けられたcodeChallengeが必要です
 export const checkCustomParams = (url: URL): boolean => {
 
-  const initialCallbackUrl = url.searchParams.get('callbackUrl');
+  const callbackUrl = url.searchParams.get('callbackUrl');
 
   // そもそもcallbackUrlがセットされていないならばチェック対象にしない
-  if (!initialCallbackUrl) return true
+  if (!callbackUrl) return true
 
   // callbackUrl がホワイトリスト内でないなら失敗
   if (
-    !config.allowedRedirectPatterns
-      .some(url => initialCallbackUrl.startsWith(url))
+    config.allowedRedirectPatterns
+      .every(url => !callbackUrl.startsWith(url))
   ) {
-    log('callbackUrl value is not allowed (/api/auth/* handler)', initialCallbackUrl)
+    log('callbackUrl value is not allowed (/api/auth/* handler)', callbackUrl)
     return false
   }
 
-  // callbackUrlが設定されているのにcodeChallengeがその中に無ければ失敗
-  const initialCodeChallenge = (new URL(initialCallbackUrl))
-    .searchParams.get('codeChallenge')
-  if (url.pathname.includes('signin') &&  !initialCodeChallenge) {
-    log('codeChallenge parameter is not set in callbackUrl')
-    return false
+  // モバイルコールバック向けかで場合分け
+  if (isMobileRedirectUrl(callbackUrl)) {
+    // モバイル向けの場合、更に内側にカスタムスキーマが設定される
+    const mobileCallbackUrl = 
+      (new URL(callbackUrl)).searchParams.get('callbackUrl')
+    if (!mobileCallbackUrl) {
+      log('mobile callback pattern detected, but no inner callbakUrl')
+      return false
+    }
+    // リダイレクト先のカスタムスキーマもホワイトリスト管理
+    if (
+      !config.allowedMobileRedirectPatterns
+        .some(url => mobileCallbackUrl.startsWith(url))
+    ) {
+      log(
+        'mobile callback pattern, innter callbackUrl detected, but not allowed: ',
+        mobileCallbackUrl
+      )
+      return false
+    }
+
+    // カスタムスキーマにはcodeChallengeパラメータが存在しているかチェック
+    const codeChallenge = 
+      (new URL(mobileCallbackUrl)).searchParams.get('codeChallenge')
+    if (!codeChallenge) {
+      log('mobile callback pattern, innter callbackUrl detected, but no codeChallenge: ', mobileCallbackUrl)
+      return false
+    }
+
+  } else {
+    // Web向け
+    // callbackUrlが設定されているのにcodeChallengeがその中に無ければ失敗
+    const codeChallenge = 
+      (new URL(callbackUrl)).searchParams.get('codeChallenge')
+    if (url.pathname.includes('signin') &&  !codeChallenge) {
+      log('codeChallenge parameter is not set in callbackUrl')
+      return false
+    }
   }
 
   return true
 }
 
+/**
+ * UrsaAuthが介入するのは認証成功時のアプリへのリダイレクト時で、
+ * それを検出するための条件判定を行います
+ * NOTE
+ * けっこう繊細なのでリファクタリング時には気を付ける
+ */
+export const isSuccessfulRedirectResponse = (
+  req: Request,
+  res: Response,
+  jwe: string | undefined
+) => {
+  if (!URL.canParse(req.url)) return false
+  const url = new URL(req.url)
+  return (
+    res.status === 302 &&
+    url.pathname.startsWith('/api/auth/callback') &&
+    jwe &&
+    res.headers.get('location')
+  )
+}
+
+/**
+ * UrsaAuth向けに設定されたcallbackUrlから、
+ * 目的とするLocationを取得します
+ */
+export const getCustomRedirectLocation = (locationUrl: URL): URL | undefined  => {
+  // TODO
+  // 実装が複雑になるが、applinkやUniversal linkを用いてcallbackできれば
+  // 複雑な設定が不要になる、要件等
+ 
+  if (!isMobileRedirectUrl(locationUrl)) {
+    // Web向けならこの時点での編集の必要はない
+    // (後でcodeChallengeを読み取ってcodeに置き換える)
+    return locationUrl
+  }
+
+  // mobile向けのコールバックならこんな形式
+  // locationUrl: ${host}/mobile?callbackUrl=custom.cheme://callback?codeChallenge...
+  const customSchemeUrlString = locationUrl.searchParams.get('callbackUrl')
+  if (!customSchemeUrlString || !URL.canParse(customSchemeUrlString)) {
+    log(
+      'mobile callback pattern detected, but callbackUrl is empty or invalid: ',
+      customSchemeUrlString
+    )
+    return undefined
+  }
+  // モバイル向けカスタムスキーマURLがホワイトリストに入るかチェック
+  const customSchemeUrl = new URL(customSchemeUrlString)
+  if (!config.allowedMobileRedirectPatterns.includes(customSchemeUrl.toString())) {
+    log(
+      'given mobile callback uri pattern is not allowed',
+      customSchemeUrl.toString(),
+    )
+    return undefined
+  }
+
+  return customSchemeUrl
+}
 
--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { checkCustomParams } from '../src/lib'
+import { config } from '../src/config'
 
 describe('custom url test', () => {
 
@@ -20,8 +21,10 @@ describe('custom url test', () => {
   }),
 
   it('non-encoded url for mobile with callbackUrl', () => {
-    const callbackUrl = 'https://next.localhost/signin?codeChallenge=12345'
-    const url = `https://auth.localhost/api/auth/signin?callbackUrl=${callbackUrl}`
+    const customSchemeCallbackUrl = 
+      `${config.allowedMobileRedirectPatterns[0]}/callback?codeChallenge=12345`
+    const callbackUrl = `http://auth.localhost/mobile?callbackUrl=${customSchemeCallbackUrl}`
+    const url = `http://auth.localhost/api/auth/signin?callbackUrl=${callbackUrl}`
     
     const result = checkCustomParams(new URL(url))
     expect(result).toBe(true)
