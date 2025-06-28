c288536c1b7b6c74495bc3088b7ebf17445ba797
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-06-29 01:04:51 +0900

    モバイル版テストアプリを追加

---
+++ examples/mobile/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/daiji/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/daiji/sources/ursa-auth/examples/mobile"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/daiji/sources/ursa-auth/examples/mobile/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_DEFINES=TFNfQVBQTElDQVRJT05fUVVFUllfU0NIRU1FUz1zZWtpcmVpLXRvZG8uZmF2ZW8tc3lzdGVtYS5uZXQ=,Q1VTVE9NX1NDSEVNRT1zZWtpcmVpLXRvZG8uZmF2ZW8tc3lzdGVtYS5uZXQ=,QVVUSF9TRVJWRVJfVVJMPWh0dHBzOi8vdXJzYS5mYXZlby1zeXN0ZW1hLm5ldA==,QVVUSF9TSUdOSU5fUEFUSD0vYXBpL2F1dGgvc2lnbmlu,QVVUSF9UT0tFTl9QQVRIPS90b2tlbg==,Q0FMTEJBQ0tfVVJMPWh0dHBzOi8vdXJzYS5mYXZlby1zeXN0ZW1hLm5ldC9tb2JpbGU/Y2FsbGJhY2tVcmw9c2VraXJlaS10b2RvLmZhdmVvLXN5c3RlbWEubmV0Oi8vY2FsbGJhY2s=,RkxVVFRFUl9WRVJTSU9OPTMuMzIuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmZiYTI0NDdlOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049OGNkMTllNTA5ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/daiji/sources/ursa-auth/examples/mobile/.dart_tool/package_config.json"
